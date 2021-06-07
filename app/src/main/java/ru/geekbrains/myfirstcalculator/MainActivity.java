a415492599ea18546b7fa2c90fbc253dd96a577f
VadimPerestoronin <v.g.perestoronin@yandex.ru>
2021-06-07 14:52:32 +0300

    Lesson04v2

--- app/src/main/java/ru/geekbrains/myfirstcalculator/MainActivity.java
+++ app/src/main/java/ru/geekbrains/myfirstcalculator/MainActivity.java
@@ -10,12 +10,15 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.Serializable;
+import java.text.BreakIterator;
 
 
 public class MainActivity extends AppCompatActivity implements Serializable {
 
     private final static String ARG_CNT = "ARG_CNT";
     private TextView numberView;
+    private TextView resultView;
+    private Calculate calculate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +26,79 @@ public class MainActivity extends AppCompatActivity implements Serializable {
         setContentView(R.layout.activity_main);
 
         numberView = findViewById(R.id.number_field);
+        resultView = findViewById(R.id.result_field);
+
+        int[] numberIds = new int[] {
+                R.id.key_0,
+                R.id.key_1,
+                R.id.key_2,
+                R.id.key_3,
+                R.id.key_4,
+                R.id.key_5,
+                R.id.key_6,
+                R.id.key_7,
+                R.id.key_8,
+                R.id.key_9
+        };
+
+        int[] actionsIds = new int[] {
+                R.id.key_plus,
+                R.id.key_minus,
+                R.id.key_multiplication,
+                R.id.key_division,
+                R.id.key_equals
+        };
 
 
-        findViewById(R.id.key_0).setOnClickListener(new View.OnClickListener() {
+        calculate = new Calculate();
 
+        View.OnClickListener numberButtonClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculate.onNumPressed(view.getId());
+                numberView.setText(calculate.getText());
+            }
+        };
+
+        View.OnClickListener actionButtonOnclickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculate.onActionPressed(view.getId());
+                numberView.setText(calculate.getText());
+
+            }
+        };
+
+        for (int i = 0; i < numberIds.length; i++) {
+            findViewById(numberIds[i]).setOnClickListener(numberButtonClickListener);
+        }
+
+        for (int i = 0; i < actionsIds.length; i++) {
+            findViewById(actionsIds[i]).setOnClickListener(actionButtonOnclickListener);
+        }
+
+        findViewById(R.id.key_clear).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculate.reset();
+                numberView.setText(calculate.getText());
+            }
+        });
+
+        findViewById(R.id.key_backspace).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculate.backspace();
+                numberView.setText(calculate.getText());
+            }
+        });
+
+        
+
+/*        findViewById(R.id.key_0).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 numberView.setText(numberView.getText().toString() + "0");
-
             }
         });
         findViewById(R.id.key_1).setOnClickListener(new View.OnClickListener() {
@@ -129,6 +197,13 @@ public class MainActivity extends AppCompatActivity implements Serializable {
                 numberView.setText(numberView.getText().toString() + "%");
             }
         });
+        findViewById(R.id.key_backspace).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String enteredNumbers = numberView.getText().toString();
+                numberView.setText(enteredNumbers.substring(0, enteredNumbers.length() -1));
+            }
+        });*/
 
 
     }
@@ -163,7 +238,7 @@ public class MainActivity extends AppCompatActivity implements Serializable {
     @Override
     protected void onSaveInstanceState(@NonNull Bundle instanceState) {
         super.onSaveInstanceState(instanceState);
-        instanceState.putSerializable(ARG_CNT, (Serializable) numberView);
+        instanceState.putParcelable(ARG_CNT, calculate);
     }
 
 
@@ -171,7 +246,8 @@ public class MainActivity extends AppCompatActivity implements Serializable {
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle instanceState) {
         super.onRestoreInstanceState(instanceState);
-        numberView = (TextView) instanceState.getSerializable(ARG_CNT);
+        calculate = instanceState.getParcelable(ARG_CNT);
+        numberView.setText(calculate.getText());
 
     }
 
